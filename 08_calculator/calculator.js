2230b28084d0b30c7bc53997bb760d177b048dbd
Rene Kumsta <rck@sequensis.de>
2023-09-05 17:09:58 +0200

    Finished all tasks

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -7,19 +7,42 @@ const subtract = function(a, b) {
 };
 
 const sum = function(arr) {
-  return arr.length === 0 ? 0 : arr[0] + arr[1];
+  console.log(arr.length);
+  if (arr.length === 1) {
+    return arr[0];
+  } else if (arr.length > 2) {
+      return arr.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue;
+      });
+  } else { return arr.length === 0 ? 0 : arr[0] + arr[1];
+  }
 };
 
-const multiply = function(a, b) {
-  return a * b;
+const multiply = function(arr) {
+  if (arr.length > 2) {
+    return arr.reduce((multiple, currentNum) => {
+      return multiple * currentNum;
+    });
+  }
+  return arr[0] * arr[1];
 };
 
-const power = function() {
-
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-
+const factorial = function(a) {
+  if (a === 0) {
+    return 1;
+  } else if (a === 1) {
+      return 1;
+  } else {
+      let cumSum = a;
+      for (let i = a - 1; i > 0; i--) {
+        cumSum *= i;
+      }
+      return cumSum;
+  }
 };
 
 // Do not edit below this line
